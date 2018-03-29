0d5ee402e9baa2370b93aa049d52edb528f63859
Sea pianist <953579842@qq.com>
2018-03-29 17:40:57 +0800

    开始进行nodejs的学习
    先看看大佬写的资料

---
+++ demo01.js
@@ -0,0 +1,15 @@
+var isTrue = function(value, callback) {
+    if (value === true) {
+        callback(null, "Value was true.");
+    } else {
+        callback(new Error("Value is not true!"));
+    }
+}
+var callback = function(error, value) {
+    if (error) {
+        return console.log(error);
+    }
+    console.log(value);
+}
+// console.log(new Error("Value is not true!"));
+isTrue(true, callback);
